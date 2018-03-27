dc7909405a8e37e3cee542f4aa1322e9de711b68
yangpengfei <yours910809@163.com>
2018-03-27 23:19:28 +0800

    Initial Commit

---
+++ yyyyy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  yyyyy
+//
+//  Created by Monkey on 2018/3/27.
+//  Copyright © 2018年 Monkey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ yyyyy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  yyyyy
+//
+//  Created by Monkey on 2018/3/27.
+//  Copyright © 2018年 Monkey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
